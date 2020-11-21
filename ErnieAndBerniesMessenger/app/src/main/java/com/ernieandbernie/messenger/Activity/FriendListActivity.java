fb1a277c2239ed1dff33e99f883ee3b613a88e3f
Laursen <201703930@post.au.dk>
2020-11-21 16:46:33 +0100

    Permission stuff

--- ErnieAndBerniesMessenger/app/src/main/java/com/ernieandbernie/messenger/Activity/FriendListActivity.java
+++ ErnieAndBerniesMessenger/app/src/main/java/com/ernieandbernie/messenger/Activity/FriendListActivity.java
@@ -26,7 +26,7 @@ public class FriendListActivity extends AppCompatActivity {
     // Register the permissions callback, which handles the user's response to the
     // system permissions dialog. Save the return value, an instance of
     // ActivityResultLauncher, as an instance variable.
-    private ActivityResultLauncher<String> requestPermissionLauncher =
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
             registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
                 if (isGranted) {
                     // Permission is granted. Continue the action or workflow in your
@@ -61,7 +61,7 @@ public class FriendListActivity extends AppCompatActivity {
             });
         } else {
             requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
-            requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION);
+            // requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION);
         }
     }
 
